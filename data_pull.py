4bce1488077f991845daf0dbb1bbd3bc1524b7ad
Kashyap Challapalli <kach4276@colorado.edu>
2022-11-27 17:49:27 -0700

    With the main page working, except for the weekends logic and the wrong data on the transactions

--- data_pull.py
+++ data_pull.py
@@ -12,17 +12,27 @@ ws = sheet1.worksheet('LITGTrans')
 df = pd.DataFrame(ws.get_all_records())
 
 
-dat = pd.DataFrame()
+totalFundValue = pd.DataFrame()
+totalFundValue['Cash'] = 156054.73
+
+# This is just dummy logic, idk if it will work at all but the code that was working before this was:
+# for n in df.iloc:
+#     totalFundValue[n.ticker] = web.DataReader(n.ticker, "yahoo", n.date, dt.date.today())['Close']*n.quantity
 
 for n in df.iloc:
-    dat[n.ticker] = web.DataReader(n.ticker, "yahoo", n.date, dt.date.today())['Close']*n.quantity
+    if n.action == "BUY":
+        totalFundValue[n.ticker] = web.DataReader(n.ticker, "yahoo", n.date, dt.date.today())['Close']*n.quantity
+        totalFundValue['Cash'] = totalFundValue['Cash'] - totalFundValue[n.ticker]
+        
+    elif n.action == "SELL":
+        totalFundValue['Cash'] = totalFundValue['Cash'] + web.DataReader(n.ticker, "yahoo", n.date, dt.date.today())['Close']*n.quantity
+        del df[n.ticker] #I dont think this line is necessary since Im not adding this specific column to the data frame in the first place
 
-dat['Sum'] = dat[list(dat.columns)].sum(axis=1) 
-dat['Cash'] = 156054.73
-dat["FundValue"] = dat['Sum']+dat['Cash']
+    
 
-dat["FundValue"].to_csv("holdings_data.csv")
+totalFundValue['Sum'] = totalFundValue[list(totalFundValue.columns)].sum(axis=1) 
 
+totalFundValue["FundValue"] = totalFundValue['Sum']+totalFundValue['Cash']
 
-
+totalFundValue["FundValue"].to_csv("holdings_data.csv")
 
--- output_sheets.py
+++ output_sheets.py
@@ -11,5 +11,10 @@ sa = gspread.service_account(filename = "testdashboard1-be5a120e4282.json")
 sheet = sa.open("total_fund")
 
 
-worksheet = sheet.worksheet("Sheet1")
+worksheet = sheet.worksheet("FundVal")
 set_with_dataframe(worksheet, df)
+
+# sc = pd.read_csv("sectors.csv")
+
+# ws = sheet.worksheet("PChart")
+# set_with_dataframe(ws, sc)
